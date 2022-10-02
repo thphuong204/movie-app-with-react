c89deb04803eb1ff17934c0ecf34ac4fcc12e4fe
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-02 15:58:59 +0700

    add App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { createTheme } from '@mui/material';
+import { grey, orange, red } from '@mui/material/colors';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import FilmThumbnailsListCarousel from './components/FilmThumbnailsListCarousel';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[800],
+      light: grey[700],
+      dark: grey[900],
+    },
+    secondary: {
+      main: red[400],
+      light: red[300],
+      dark: red[800],
+    },
+    warning: {
+      main: orange[400]
+    },
+    background: {
+      default: grey[900],
+    },
+    spacing: { xs: 2, sm: 3, md: 5 },
+  },
+  components: {
+    // Name of the component
+    MuiPaginationItem: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          color: 'white',
+        },
+      },
+    },
+    
+  },
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilmThumbnailsListCarousel/>
     </div>
   );
 }
