4533912b177d26b0269411db246d7ce737a406bd
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-09 14:21:44 +0700

    passing context to @TestApiComponent for testing

--- src/components/TestApiComponent.js
+++ src/components/TestApiComponent.js
@@ -1,17 +1,12 @@
-import { useEffect, useState } from "react"
-import { v3GetMovieDetails } from "../apis/tmdb";
+import { useContext, useEffect, useState } from "react"
+import { apiV3Discover, apiV3GetMovieDetails } from "../apis/tmdb";
+import { FilmContext } from "./../App";
 
 export const TestApiComponent = () => {
-
-    const [movieDetails, setMovieDetails] = useState(null);
-
-    useEffect(async () => {
-        const movie = await v3GetMovieDetails();
-        setMovieDetails(movie);
-    }, [])
-
-    return <div style={{ color: "red" }}>
+    const { movieDetails, setMovieDetails, movieArrays, setMovieArrays } = useContext(FilmContext);
+    return (
+    <div className="textdetial" style={{ color: "red" }}>
         TestApiComponent
-        <h1>{movieDetails?.title}</h1>
-    </div>
+        <h1>{` ${movieDetails?.title} bi sao cho movieDetails`}</h1>
+    </div>)
 }
