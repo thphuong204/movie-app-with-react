27bf016b4613d54a5fad80fa4495cb09b58d4fe0
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-09 23:10:56 +0700

    import Slider from react-slick for carousel in @FilmThumbnailsList component (need to adjust CSS for rendering)

--- src/components/FilmThumbnailsList.js
+++ src/components/FilmThumbnailsList.js
@@ -2,39 +2,30 @@ import React, { useContext } from 'react'
 import FilmThumbnail from './FilmThumbnail';
 import './../App.css';
 import { FilmContext } from '../App';
-import Carousel from 'react-multi-carousel';
-
-import "react-multi-carousel/lib/styles.css";
-
+import Slider from "react-slick";
 
 function FilmThumbnailsList() {
   const { movieDetails, setMovieDetails, movieArrays, setMovieArrays } = useContext(FilmContext);
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+  const settings = {
+    dots: true,
+    infinite: true, 
+    speed:1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: false
   };
 
   return (
     <div className="filmThumbnailsList">
-
-      {movieArrays?.map((movieObject) => {
-        return <FilmThumbnail key={movieObject?.id} movieObject={movieObject} />
-      })}
+       <Slider {...settings}>
+          {movieArrays?.map((movieObject)=>{
+            return (
+                <FilmThumbnail key={movieObject?.id} movieObject={movieObject}/>
+              
+              )
+          })}
+          </Slider>
+   
     </div>
   )
 }
