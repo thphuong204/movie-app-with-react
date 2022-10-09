80a03fbd83aad34e84a0ead1e9cc5201394a8e9f
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-09 14:19:49 +0700

    interact api tmdb to get more information at once fetching

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -7,17 +7,20 @@ const request = axios.create({
     baseURL: API_V3_BASE_URL,
     timeout: 1000,
     params: {
-        api_key: API_V3_KEY
+        api_key: API_V3_KEY,
+        page: 2,
+        append_to_response:"videos,images,credits"
     }
 });
 
-export const v3Discover = async (page, pageSize, searchQuery) => {
+export const apiV3Discover = async (page, pageSize, searchQuery) => {
     console.log('discovering movie')
     const response = await request.get('discover/movie');
     console.log(response.data);
+    return response.data;
 }
 
-export const v3GetMovieDetails = async (movieId) => {
+export const apiV3GetMovieDetails = async (movieId) => {
     console.log('getting movie details')
     const response = await request.get('movie/760161');
     console.log(response.data);
