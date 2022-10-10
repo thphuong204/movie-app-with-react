922e9a7d126e5b83f3567ee1444a01aeecb8acbc
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-10 18:00:22 +0700

    render video @FilmDetailsPage

--- src/pages/FilmDetailsPage.js
+++ src/pages/FilmDetailsPage.js
@@ -16,7 +16,7 @@ function FilmDetailsPage() {
   const movieGenresOriginalArr = movieDetails?.genres;
   const productionCompaniesOriginalArr = movieDetails?.production_companies;
   const castOriginalArr = movieDetails?.credits?.cast
-
+  const videoOriginalArr = movieDetails?.videos?.results
 
   return (
     <div id="filmDetails" style={{backgroundColor:"grey"}}>
@@ -25,26 +25,40 @@ function FilmDetailsPage() {
         </div>
         <Container fluid className="filmDetailsBody">
             <Container fluid className="filmDetailsBody_header px-0">
-              <Image className="filmDetailsBody_image"
-              image="./../../public/logo512.png"
-              alt="img of film">
-              </Image >
+              <Row style={{width:"100%", margin:"0px", justifyContent:"center"}}>
+                <Col lg={4} md={6} sx={12} style={{padding:"0px"}}>
+                <Image className="filmDetailsBody_image"
+                src={`https://image.tmdb.org/t/p/original${movieImage}`}
+                alt="img of film">
+                </Image >
+              </Col>
+              </Row>
               <Container fluid className="filmDetailsBody_title">{movieTitle}</Container>
             </Container>
             <Container fluid className="filmDetailsBody_description">
               <Row className="w-100">
                 <Col className="description"  >
                   <Container className="description-type">
-                    {`Production Companies:`}
+                  <span>Production Companies: </span>
+                  {productionCompaniesOriginalArr?.map((productionCompObject,i)=> {
+                    return (
+                      <span key={i}>{`${productionCompObject?.name}, `}</span>
+                    )
+                  })}
                   </Container>
                   <Container className="description-genre" >
                    <span>Genres: </span> 
                    {movieGenresOriginalArr?.map((genreObject,i)=> {
-                    return (<span key={i}>{`${genreObject?.name} `}</span>)
+                    return (<span key={i}>{`${genreObject?.name}, `}</span>)
                    })}
                   </Container>
                   <Container className="description-cast" >
-                    Cast:
+                  <span>Casts: </span> 
+                  {castOriginalArr?.slice(0,5)?.map((castObject,i)=>{
+                    return (
+                      <span key={i}>{`${castObject?.name}, `}</span>
+                    )
+                  })}
                   </Container>
                   <Container className="description-content">
                     {`Description: ${movieOverview}`}
@@ -53,14 +67,18 @@ function FilmDetailsPage() {
                 
                 <Col  className="episode-guide">
                     <Container className="episode-guide-title">Episode guide:</Container> 
-                    <Container className="duration">Duration: 1hour 30 minutes</Container> 
-                    <Container className="season-name">Season 1
+                    <Container className="season-name">
                       <ul>
-                        <li className="episode-name">Ep1</li>
+                        {videoOriginalArr?.slice(0,1)?.map((videoObject,i)=> {
+                          return (
+                            <li className="episode-name" key={i}>
+                              <a href="">
+                                {videoObject?.name}
+                              </a>
+                            </li>
+                          )
+                        })}
                       </ul>
-                    </Container>
-                    <Container className="trailer">
-                      Trailer
                     </Container>
                 </Col>
                 </Row>
