67f9b7bc5a979b72978f5c5f3552dd80401fb997
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-10 15:59:01 +0700

    add FilmByGenresPage

---
+++ src/pages/FilmByGenresPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FilmAppBar from '../components/FilmAppBar'
+import FilmGenresResults from '../components/FilmGenresResults'
+
+function FilmByGenresPage() {
+  return (
+    <div>
+        <FilmAppBar/>
+        <FilmGenresResults/>
+    </div>
+  )
+}
+
+export default FilmByGenresPage
