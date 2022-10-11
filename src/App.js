68893f470a1c5c638a6e23dbfd892e5395f40bb8
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-11 09:36:11 +0700

    declare function for fetching movie arrays by genre

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import VideoPlayerPage from './pages/VideoPlayerPage';
 import HomePage from './pages/HomePage';
 import FilmByGenresPage from './pages/FilmByGenresPage';
 import { TestApiComponent } from './components/TestApiComponent';
-import { apiV3Discover, apiV3GetMovieDetails } from './apis/tmdb';
+import { apiV3Discover, apiV3DiscoverAction, apiV3DiscoverCommedy, apiV3DiscoverDramma, apiV3GetMovieDetails } from './apis/tmdb';
 
 
 const theme = createTheme({
@@ -52,12 +52,42 @@ const FilmContext = createContext();
 function App() {
   const [movieDetails, setMovieDetails] = useState(null);
   const [movieArrays, setMovieArrays] = useState(null);
-
+  const [movieActionArrays,setActionArrays] = useState(null);
+  const [movieCommedyArrays,setCommedyArrays] = useState(null);
+  const [movieDrammaArrays,setDrammaArrays] = useState(null);
+  
   useEffect(() => {
     //useEffect warning error when putting async before useEffect arrow function => fixed
     async function setMovieArr() {
       const movieArr = await apiV3Discover();
       setMovieArrays(movieArr.results);
+    }
+    setMovieArr();
+  }, [])
+
+  useEffect(() => {
+    //useEffect warning error when putting async before useEffect arrow function => fixed
+    async function setMovieArr() {
+      const movieActionArr = await apiV3DiscoverAction();
+      setActionArrays(movieActionArr.results);
+    }
+    setMovieArr();
+  }, [])
+
+  useEffect(() => {
+    //useEffect warning error when putting async before useEffect arrow function => fixed
+    async function setMovieArr() {
+      const movieCommedyArr = await apiV3DiscoverCommedy();
+      setCommedyArrays(movieCommedyArr.results);
+    }
+    setMovieArr();
+  }, [])
+
+  useEffect(() => {
+    //useEffect warning error when putting async before useEffect arrow function => fixed
+    async function setMovieArr() {
+      const movieDrammaArr = await apiV3DiscoverDramma();
+      setDrammaArrays(movieDrammaArr.results);
     }
     setMovieArr();
   }, [])
--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -13,9 +13,62 @@ const request = axios.create({
     }
 });
 
+const requestAction = axios.create({
+    baseURL: API_V3_BASE_URL,
+    timeout: 1000,
+    params: {
+        api_key: API_V3_KEY,
+        page: 2,
+        append_to_response:"videos,images,credits",
+        with_genres: 28
+    }
+});
+
+const requestDramma = axios.create({
+    baseURL: API_V3_BASE_URL,
+    timeout: 1000,
+    params: {
+        api_key: API_V3_KEY,
+        page: 2,
+        append_to_response:"videos,images,credits",
+        with_genres: 18
+    }
+});
+const requestCommedy = axios.create({
+    baseURL: API_V3_BASE_URL,
+    timeout: 1000,
+    params: {
+        api_key: API_V3_KEY,
+        page: 2,
+        append_to_response:"videos,images,credits",
+        with_genres: 35
+    }
+});
+
 export const apiV3Discover = async (page, pageSize, searchQuery) => {
     console.log('discovering movie')
     const response = await request.get('discover/movie');
+    console.log(response.data);
+    return response.data;
+}
+
+export const apiV3DiscoverAction = async (page, pageSize, searchQuery) => {
+    console.log('discovering action movie')
+    const response = await requestAction.get('discover/movie');
+    console.log(response.data);
+    return response.data;
+}
+
+export const apiV3DiscoverCommedy = async (page, pageSize, searchQuery) => {
+    console.log('discovering action movie')
+    const response = await requestCommedy.get('discover/movie');
+    console.log(response.data);
+    return response.data;
+}
+
+export const apiV3DiscoverDramma = async (page, pageSize, searchQuery) => {
+    console.log('discovering action movie')
+    const response = await requestDramma.get('discover/movie');
     console.log(response.data);
     return response.data;
 }
@@ -26,3 +79,4 @@ export const apiV3GetMovieDetails = async (movieId) => {
     console.log(response.data);
     return response.data;
 }
+
