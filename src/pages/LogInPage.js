31250cfbe77d3372e400d071e95f06f0a43ed762
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-12 21:10:11 +0700

    Navigate to /home on succesful login

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
 import "./formStyles.css";
 import AppBar from '@mui/material/AppBar';
@@ -13,139 +12,148 @@ import { createTheme, ThemeProvider } from '@mui/material';
 import { grey, orange, red } from '@mui/material/colors';
 import { login } from '../apis/auth';
 import { FilmContext } from '../App';
+import { useNavigate } from 'react-router-dom';
 
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: grey[800],
-        light: grey[700],
-        dark: grey[900],
-      },
-      secondary: {
-        main: red[400],
-        light: red[300],
-        dark: red[800],
-      },
-      warning: {
-        main: orange[400]
-      },
-      background: {
-        default: grey[900],
-      },
-      spacing: { xs: 2, sm: 3, md: 5 },
+  palette: {
+    primary: {
+      main: grey[800],
+      light: grey[700],
+      dark: grey[900],
     },
-    components: {
-      // Name of the component
-      MuiInputBase: {
-        styleOverrides: {
-          root: {
-            height: "30px",
-          }
-        }
-      },
-      MuiAppBar: {
-        styleOverrides: {
-          root: {
-            backgroundColor:"#212121"
-          }
+    secondary: {
+      main: red[400],
+      light: red[300],
+      dark: red[800],
+    },
+    warning: {
+      main: orange[400]
+    },
+    background: {
+      default: grey[900],
+    },
+    spacing: { xs: 2, sm: 3, md: 5 },
+  },
+  components: {
+    // Name of the component
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          height: "30px",
         }
       }
     },
-  })
-
-
-function LogInPage() {
-
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors }
-      } = useForm();
-    
-      const onSubmit = (data) => {
-        alert(JSON.stringify(data));
-      }; // your form submit function which will invoke after successful validation
-    
-      console.log(watch("example")); // you can watch individual input by pass the name of the input
-      const [email, setEmail] = useState("phuong@gmail.com");
-      const [password, setPassword] = useState("123456");
-      const { setLoggedIn } = useContext(FilmContext);
-
-      const handleLogin = () => {
-        const token = login(email, password);
-        console.log('token', token)
-        if (token) {
-          localStorage.setItem('token', JSON.stringify(token));
-          setLoggedIn(true);
-        } else {
-          localStorage.removeItem('token');
-          setLoggedIn(false);
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#212121"
         }
-      };
+      }
+    }
+  },
+})
+
+
+const LogInPage = (props) => {
+
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  }; // your form submit function which will invoke after successful validation
+
+  console.log(watch("example")); // you can watch individual input by pass the name of the input
+  const [email, setEmail] = useState("phuong@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const { setLoggedIn } = useContext(FilmContext);
+
+  const handleLogin = () => {
+    const token = login(email, password);
+    console.log('token', token)
+    if (token) {
+      localStorage.setItem('token', JSON.stringify(token));
+      setLoggedIn(true);
+      navigate('/home')
+    } else {
+      localStorage.removeItem('token');
+      setLoggedIn(false);
+    }
+  };
 
   return (
     <ThemeProvider theme={theme}>
-        <div>
+      <div>
         <AppBar position="static">
-            <Toolbar>
+          <Toolbar>
             <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
             >
-                <LiveTvIcon sx={{ fontSize: 50 }}/>
+              <LiveTvIcon sx={{ fontSize: 50 }} />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             </Typography>
-            <Button color="inherit" style={{backgroundColor:"#FFA500", borderColor: "#FFA500", 
-            fontFamily: "Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue"
-    , fontSize:"16px" , color: "black", fontWeight:"bold"}}>Sign up</Button>
-            </Toolbar>
+            <Button color="inherit" style={{
+              backgroundColor: "#FFA500", borderColor: "#FFA500",
+              fontFamily: "Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue"
+              , fontSize: "16px", color: "black", fontWeight: "bold"
+            }}>Sign up</Button>
+          </Toolbar>
         </AppBar>
-        </div>
+      </div>
 
-        <div id="formLogIn">
+      <div id="formLogIn">
         <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label style={{color:"#fff"}} >Email address</Form.Label>
-            <Form.Control 
-            type="email" 
-            placeholder="Enter email" 
-            defaultValue={email}
-            onChange={(event) => setEmail(event.target.value)}/>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label style={{ color: "#fff" }} >Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              defaultValue={email}
+              onChange={(event) => setEmail(event.target.value)} />
             <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+              We'll never share your email with anyone else.
             </Form.Text>
-        </Form.Group>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label style={{color:"#fff"}} >Password</Form.Label>
-            <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            defaultValue={password}
-            onChange={(event) => setPassword(event.target.value)}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" style={{color:"#fff"}} label="Remember me" />
-        </Form.Group>
-        <Button 
-          variant="primary" 
-          type="submit" 
-          style={{backgroundColor:"#FFA500", 
-          borderColor: "#FFA500", 
-          color:"black", 
-          fontWeight:"bold"}}
-          onClick={handleLogin}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label style={{ color: "#fff" }} >Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              defaultValue={password}
+              onChange={(event) => setPassword(event.target.value)} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" style={{ color: "#fff" }} label="Remember me" />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="submit"
+            style={{
+              backgroundColor: "#FFA500",
+              borderColor: "#FFA500",
+              color: "black",
+              fontWeight: "bold"
+            }}
+            onClick={handleLogin}
           >
             Submit
-        </Button>
+          </Button>
         </Form>
-        </div>
+      </div>
     </ThemeProvider>
   );
 }
